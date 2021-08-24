734bd276c0ccfb0ca311f32035c2883cc90c098d
aquib <shaikhaquib394@gmail.com>
2021-08-24 14:27:46 +0530

    Initialize project, add layouts/sheet.py, manager, init files.

---
+++ manager.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import N, S, E, W
+import pandas
+
+class HomeDepotManger(tk.Tk):
+    def __init__(self):
+        self.rowconfigure(0, weight=3)
+        self.columnconfigure(0, weight=3)
+
+
+
+
