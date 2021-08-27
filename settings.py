97b8fab66b33e14e0825a76c4489690afcb8f838
Aquib <shaikhaquib394@gmail.com>
2021-08-27 16:35:03 +0530

    Add settings.py with settings of app.

---
+++ settings.py
@@ -0,0 +1,7 @@
+import os
+
+website     = "https://www.homedepot.com/"
+browser     = "Firefox"
+headless    = True
+cache_file  = "app_data.json"
+xpaths_file = os.path.abspath(os.path.join("services", "xpaths.json"))
