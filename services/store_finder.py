9637ab3ff43cdc3a6cbf3b12ff85af3e93a26eb9
Aquib <shaikhaquib394@gmail.com>
2021-08-27 16:37:00 +0530

    Add Store Finder to find stores.

---
+++ services/store_finder.py
@@ -0,0 +1,13 @@
+# StoreFinder
+# Author : Shaikh Aquib
+# Date   : August 2021
+
+
+class StoreFinder:
+    """Finds the HomeDepot store by store number or zip code."""
+    def __init__(self, bot) -> None:
+        """
+        bot: bot.BotMaker
+            BotMaker object with home depot page loaded.
+        """
+        pass
