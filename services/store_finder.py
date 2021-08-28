fe0de5f5664a8462c2c1526e309e54439e6fcba2
Aquib <shaikhaquib394@gmail.com>
2021-08-28 13:59:40 +0530

    Add method to select store by number.

--- services/store_finder.py
+++ services/store_finder.py
@@ -2,12 +2,41 @@
 # Author : Shaikh Aquib
 # Date   : August 2021
 
+import sys
+sys.path.append("../")
+
+import time
+import settings
+from services.json_helper import JSONHelper
+
 
 class StoreFinder:
     """Finds the HomeDepot store by store number or zip code."""
+
     def __init__(self, bot) -> None:
         """
         bot: bot.BotMaker
             BotMaker object with home depot page loaded.
         """
-        pass
+        self.bot = bot
+        self.helper  = JSONHelper()
+        self.xpaths = self.helper.get_json_data(settings.xpaths_file)
+
+
+    def select_store_by_number(self, store_number: str):
+        """Selects store based on number."""
+        self.bot.move(settings.store_search_url)
+        self.bot.get_element(self.xpaths["store_search_box"]).clear()
+        self.bot.get_element(self.xpaths["store_search_box"]).send_keys(store_number)
+        self.bot.get_element(self.xpaths["store_search_btn"]).click()
+        time.sleep(2)
+
+        # Search all store elements if #store_number is found 
+        # then click the shop the store element
+        stores = self.bot.get_elements(self.xpaths["store_list_section_elems"])
+        for store in stores:
+            if '#'+str(store_number) in store.text:
+                self.bot.get_element(self.xpaths["shop_store_btn" ], 
+                        elem=store).click()
+                print(f"[+] Selected #{store_number} store.")
+
