1dc335268453ceeffde3598beefb5e19fc42166b
Aquib <shaikhaquib394@gmail.com>
2021-08-28 19:51:26 +0530

    Add Scan Price tool to menu, remove unnecessary menus.

--- layouts/menus/menubar.py
+++ layouts/menus/menubar.py
@@ -25,24 +25,21 @@ class MenuBar(tk.Menu):
         # Add the first 3 menus
         filemenu  = tk.Menu(self, tearoff=0)
         editmenu  = tk.Menu(self, tearoff=0)
-        chartmenu = tk.Menu(self, tearoff=0)
         toolsmenu = tk.Menu(self, tearoff=0)
 
         # Give names to above menus
         self.add_cascade(label="File", menu=filemenu)
         self.add_cascade(label="Edit", menu=editmenu)
-        self.add_cascade(label="3D Chart", menu=chartmenu)
         self.add_cascade(label="Tools", menu=toolsmenu)
 
         # SubMenu inside editmenu
         insertmenu = tk.Menu(editmenu, tearoff=0)
         deletemenu = tk.Menu(editmenu, tearoff=0)
-        clearmenu  = tk.Menu(editmenu, tearoff=0)
 
         # Add labels to the above submenus inside edit menu
         editmenu.add_cascade(label="Insert", menu=insertmenu)
         editmenu.add_cascade(label="Delete", menu=deletemenu)
-        editmenu.add_cascade(label="Clear", menu=clearmenu)
+        editmenu.add_command(label="Clear", command=self.master.sheet_worker.clear_column)
 
         # Row, column options for insert, delete and clear
         insertmenu.add_command(label="Row", command=None)
@@ -51,10 +48,9 @@ class MenuBar(tk.Menu):
         deletemenu.add_command(label="Row", command=None)
         deletemenu.add_command(label="Column", command=self.master.sheet_worker.delete_column)
 
-        #clearmenu.add_command(label="Row", command=None)
-        clearmenu.add_command(label="Column", command=self.master.sheet_worker.clear_column)
-
         filemenu.add_command(label="Open", command=self.master.open_file)
         filemenu.add_command(label="Save", command=self.master.sheet_worker.save_file)
         filemenu.add_command(label="Save as", command=self.master.save_file_as)
         
+        # Tools
+        toolsmenu.add_command(label="Scan Price", command=self.master.scan_price)
