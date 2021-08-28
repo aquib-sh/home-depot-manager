08b41478c1bc5b6244ac20c6262f4ec059aee58d
Aquib <shaikhaquib394@gmail.com>
2021-08-28 14:00:02 +0530

    Add file to store settings.

--- settings.py
+++ settings.py
@@ -1,7 +1,8 @@
 import os
 
-website     = "https://www.homedepot.com/"
-browser     = "Firefox"
-headless    = True
-cache_file  = "app_data.json"
-xpaths_file = os.path.abspath(os.path.join("services", "xpaths.json"))
+website          = "https://www.homedepot.com/"
+store_search_url = "https://www.homedepot.com/l/" 
+browser          = "Firefox"
+headless         = False
+cache_file       = "app_data.json"
+xpaths_file      = os.path.abspath(os.path.join("services", "xpaths.json"))
