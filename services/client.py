e874b319baffef059bc0ea8b03d4e071ef69a445
Aquib <shaikhaquib394@gmail.com>
2021-08-30 19:14:42 +0530

    Return price with decimals.

--- services/client.py
+++ services/client.py
@@ -6,6 +6,8 @@ import settings
 from bot import BotMaker
 from services.store_finder import StoreFinder
 from services.json_helper import JSONHelper
+from selenium.common.exceptions import StaleElementReferenceException
+
 
 class HomeDepotClient:
 
@@ -18,12 +20,40 @@ class HomeDepotClient:
 
     def search_by_sku(self, sku: str) -> None:
         """Searches for the product by the SKU param."""
-        self.bot.get_element(self.xpaths['search_box']).clear()
-        self.bot.get_element(self.xpaths['search_box']).send_keys(sku)
-        self.bot.get_element(self.xpaths['search_btn']).click()
-        
+        while True:
+            try:
+                self.bot.get_element(self.xpaths['search_box']).clear()
+                break
+            except StaleElementReferenceException:
+                self.bot.get_element(self.xpaths['search_box']).clear()
+            print("entry 1")
+        while True:
+            try:
+                self.bot.get_element(self.xpaths['search_box']).send_keys(sku)
+                break
+            except StaleElementReferenceException:
+                self.bot.get_element(self.xpaths['search_box']).send_keys(sku)
+                print("entry 2")
+        while True:
+            try:
+                self.bot.get_element(self.xpaths['search_btn']).click()
+                break
+            except StaleElementReferenceException:
+                self.bot.get_element(self.xpaths['search_btn']).click()
+            print("entry 3")
+
     def get_price(self) -> str:
         """Returns the product price present on current page."""
         time.sleep(2)
         price_elems = self.bot.get_elements(self.xpaths['price_elems'])
-        return str(price_elems[0].text + price_elems[1].text)
+        price = str(price_elems[0].text + price_elems[1].text)
+
+        if len(price_elems) == 3:
+            price_elems = self.bot.get_elements(self.xpaths['price_elems'])
+            price = str(price_elems[0].text + price_elems[1].text + "."+price_elems[2].text)
+        
+        return price
+
+    def select_store_by_number(self, store_number: str) -> None:
+        """Selects the store using StoreFinder."""
+        self.locater.select_store_by_number(store_number)
